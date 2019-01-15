e2066db6f80244a48fbe5d3da615e4964802291f
4class-15 <4class-15@4class-15-PC>
2019-01-15 17:53:34 +0900

    lombok정리

---
+++ SpringMVC02/src/test/java/com/tis/myapp/TestDTO.java
@@ -0,0 +1,24 @@
+package com.tis.myapp;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Component
+@Data
+@Getter
+@ToString
+@AllArgsConstructor//모든 매개변수를 받는 생성자를 만듦
+@RequiredArgsConstructor //@NonNull이나 final이 붙은 변수에 대한 생성자를 만듦
+public class TestDTO {
+	@NonNull
+	private String name;
+	private int age;
+	private String dept;
+
+}
