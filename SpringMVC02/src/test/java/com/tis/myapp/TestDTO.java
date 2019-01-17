7e7c40d91a3924c2a4e3401e791df5e74d0e818f
4class-15 <4class-15@4class-15-PC>
2019-01-17 17:51:14 +0900

    오라클과 연동하는 JDBC테스트와 HikariCP 커넥션풀/DataSource 테스트 추가

--- SpringMVC02/src/test/java/com/tis/myapp/TestDTO.java
+++ SpringMVC02/src/test/java/com/tis/myapp/TestDTO.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -13,6 +14,7 @@ import lombok.ToString;
 @Data
 @Getter
 @ToString
+@NoArgsConstructor
 @AllArgsConstructor//모든 매개변수를 받는 생성자를 만듦
 @RequiredArgsConstructor //@NonNull이나 final이 붙은 변수에 대한 생성자를 만듦
 public class TestDTO {
